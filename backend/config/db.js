be74141c0c0d382ca0b1fd87979aa1eb8fa9fbb5
Jun wang <wang.jun6@northeastern.edu>
2025-03-27 13:44:40 -0600

    db.js: Fix connection string for MongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,7 @@ import { ENV_VARS } from "./envVars.js";
 
 export const connectDB = async () => {
 	try {
+		console.log("db_uri",ENV_VARS.MONGO_URI);
 		const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
 		console.log("MongoDB connected: " + conn.connection.host);
 	} catch (error) {
